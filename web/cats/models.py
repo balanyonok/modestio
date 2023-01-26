fd46eb872af01845ea7b1de41806e43b7b85df0a
Yana Balanenko <balanyonok@gmail.com>
2023-01-26 18:06:07 +0100

    Cat model created

---
+++ web/cats/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+#Cats
+class Cat(models.Model):
+    SEX_CHOICES = [('m','male'), ('f','female')]
+    
+    name = models.CharField(max_length=20)
+    # we need years and months as well if its a kitten
+    # don't know how to make it yet
+    age = models.IntegerField()    
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES)
+    health_status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+        
