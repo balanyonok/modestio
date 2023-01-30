1ba2de7aa293686f1ab5c81ef56d8f6ff53428b5
Yana Balanenko <balanyonok@gmail.com>
2023-01-30 22:04:22 +0100

    Updated Cats app urls

--- web/cats/models.py
+++ web/cats/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 from datetime import date
 # Create your models here.
 
@@ -6,15 +8,16 @@ class Cat(models.Model):
     SEX_CHOICES = [('m','male'), ('f','female')]
     
     name = models.CharField(max_length=20)
-    # we need years and months as well if its a kitten
-    # don't know how to make it yet
-    #age = models.IntegerField()
-    approximate_date_of_birth = models.DateField(default = date(1900, 1, 1))  
+    approximate_date_of_birth = models.DateField(default = date(1900, 1, 1)) 
+    # age =
     sex = models.CharField(max_length=6, choices=SEX_CHOICES)
     health_status = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('cats:profile', kwargs={'pk' : self.pk})
 
 
 class Photo(models.Model):
