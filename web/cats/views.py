2559cc78cd465266877945fb105e9725b04c9912
Yana Balanenko <balanyonok@gmail.com>
2023-01-30 20:10:58 +0100

    Created views for Cats app

--- web/cats/views.py
+++ web/cats/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Cat
 
-# Create your views here.
+
+class AvailableCatsView(generic.ListView):
+    """
+    Return the list of cats submitted by admin(pani Kockova)
+    with every cat's name, age and 1 picture.
+    """
+    model = Cat
+    template_name = 'cats/cats_for_adoption.html'
+    context_object_name = 'available_cats'
+    
+    def get_queryset(self):
+        return Cat.objects.all
+
+
+class CatProfileView(generic.DetailView):
+    """
+    Return the chosen cat profile
+    with all the details including up to 10 pictures.
+    """
+    model = Cat
+    template_name = 'cats/cat_profile.html'
+
+    def view_cat_profile(request, primary_key):
+        cat_profile = get_object_or_404(Cat, pk=primary_key)
+        return render(request, template_name, context={'cat': cat_profile})
+
+    
