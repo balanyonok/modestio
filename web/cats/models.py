2012bb8282474bfa766c4c327378f0de96121af8
Yana Balanenko <balanyonok@gmail.com>
2023-01-31 18:46:54 +0100

    Added calculate_age function to Cat model with tests

--- web/cats/models.py
+++ web/cats/models.py
@@ -2,16 +2,27 @@ from django.db import models
 from django.urls import reverse
 
 from datetime import date
-# Create your models here.
+
 
 class Cat(models.Model):
     SEX_CHOICES = [('m','male'), ('f','female')]
     
     name = models.CharField(max_length=20)
     approximate_date_of_birth = models.DateField(default = date(1900, 1, 1)) 
-    # age =
     sex = models.CharField(max_length=6, choices=SEX_CHOICES)
     health_status = models.CharField(max_length=100)
+    
+    def calculate_age(self):
+        birthdate = self.approximate_date_of_birth
+        today = date.today()
+        age_months = (today - birthdate).days /30
+
+        if age_months >= 1 and age_months < 12:
+            self.age = '{:.0f} months'.format(age_months)
+        else:
+            age_years = age_months /12
+            self.age = '{:.0f} years'.format(age_years)
+        return self.age
     
     def __str__(self):
         return self.name
@@ -22,4 +33,4 @@ class Cat(models.Model):
 
 class Photo(models.Model):
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField()
+    image = models.ImageField(upload_to='cats/photos')
--- web/cats/tests.py
+++ web/cats/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Cat
+from datetime import date
 
-# Create your tests here.
+class CatModelTest(TestCase):
+
+    def test_calculate_age_with_1month_old_kitten(self):
+        # Returns calculated age of a kitten in months (string)
+        kitten = Cat(approximate_date_of_birth = date(2023, 1, 1))
+        self.assertEqual(kitten.calculate_age(), '1 months')
+
+
+    def test_calculate_age_3years_old_cat(self):
+        # Returns calculated age of a the cat in years (string)
+        adult_cat = Cat(approximate_date_of_birth = date(2020, 1, 1))
+        self.assertEqual(adult_cat.calculate_age(), '3 years')
