b79b028ce04b92c25741cf3bfe12542ae8195914
Yana Balanenko <balanyonok@gmail.com>
2023-02-02 18:43:00 +0100

    fix: calculate_age function

--- web/cats/models.py
+++ web/cats/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 
 from datetime import date
+import re
+import math
 
 
 class Cat(models.Model):
@@ -18,11 +20,18 @@ class Cat(models.Model):
         age_months = (today - birthdate).days /30
 
         if age_months >= 1 and age_months < 12:
-            self.age = '{:.0f} months'.format(age_months)
+            # For a kitten we need it's age in months.
+            # Min. age of a kitten for adoption is 1 month, so we don't expect user to submit a kitten younger than that.
+            return '{:.0f} months'.format(age_months)
         else:
+            # For an adult cat (older than 1 year) we need it's age in years.
             age_years = age_months /12
-            self.age = '{:.0f} years'.format(age_years)
-        return self.age
+            if re.search(r"\.[0-4]", str(age_years)):
+                # Age is rounded down if the first decimal of age_years is < 5
+                return '{:.0f} years'.format(age_years)
+            else:
+                # Age is rounded up if the first decimal of age_years is >= 5 
+                return '{:.0f},5 years'.format(math.floor(age_years))
     
     def __str__(self):
         return self.name
