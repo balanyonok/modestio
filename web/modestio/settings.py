53437110f266990234e5ebc176184e0108c44595
Yana Balanenko <balanyonok@gmail.com>
2023-03-08 21:32:37 +0100

    fix: database hostname

--- web/modestio/settings.py
+++ web/modestio/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
